b983d46ed72157b1c629744ebdc08c83df4fbc90
Theodore Wahle <theodorewahle@gmail.com>
2019-04-30 15:23:57 -0400

    Completed the workshop

--- server/src/server.js
+++ server/src/server.js
@@ -33,7 +33,7 @@ app.post('/', upload.single('file'), (req, res, next) => {
     body: fs.createReadStream(req.file.path),
     auth: {
       user: 'apikey',
-      pass: '<YOUR API KEY>',
+      pass: 'LKFoU1AZuOnxox8H3QCziPXuitLy8z7-BHgjBlro4Mr4',
     },
   }, (error, response, body) => {
     if (error) {
